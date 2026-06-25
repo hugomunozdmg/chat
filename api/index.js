63cd67f37413bf17ebdc3a974f7314d62143d272
Hugo <hugomdmg@profesorestrazos.net>
2026-06-25 16:51:01 +0200

    add login and styles

--- api/index.js
+++ api/index.js
@@ -1,54 +1,53 @@
 import e from "express";
-import cors from "cors"
+import cors from "cors";
 
+const app = e();
 
-const app = e()
+app.use(cors());
+app.use(e.urlencoded({ extended: false }));
+app.use(e.json());
 
-app.use(cors())
-app.use(e.urlencoded({ extended: false }))
-app.use(e.json())
-
-const users = ["felipe", "jose", "andres"]
-const messages = []
-
+const users = ["felipe", "jose", "andres", "rafa"];
+const messages = [];
 
 app.get("/", (req, res) => {
-
-    res.send("hello world")
-})
-
+  res.send("hello world");
+});
 
 app.post("/register-user", (req, res) => {
-
-    users.push(req.body.user)
-    res.send({ user: req.body.user })
-})
-
-
-app.post("/register-message", (req, res) => {
-
-    messages.push(req.body.message)
-    res.send(req.body.message)
-
-})
+  users.push(req.body.user);
+  res.send({ user: req.body.user });
+});
 
 app.get("/users", (req, res) => {
-    res.send({ users: users })
-})
+  res.send({ users: users });
+});
 
+app.post("/login", (req, res) => {
+  const user = req.body.user;
+  const found = users.find((u) => u == user);
+
+  res.send({ user: found });
+});
+
+app.post("/register-message", (req, res) => {
+  messages.push(req.body.message);
+  res.send(req.body.message);
+});
 
 app.post("/messages", (req, res) => {
-    console.log(req.body)
-    let foundMessages = []
-    let user1 = req.body.user1
-    let user2 = req.body.user2
-    messages.forEach(m => {
-        if ((m.user1 == user1 && m.user2 == user2) || (m.user2 == user1 && m.user1 == user2)) {
-            foundMessages.push(m)
-        }
-    })
-    res.send({ messages: foundMessages })
-})
+  let foundMessages = [];
+  let user1 = req.body.user1;
+  let user2 = req.body.user2;
+  messages.forEach((m) => {
+    if (
+      (m.user1 == user1 && m.user2 == user2) ||
+      (m.user2 == user1 && m.user1 == user2)
+    ) {
+      foundMessages.push(m);
+    }
+  });
+  res.send({ messages: foundMessages });
+});
 
-
-app.listen(3000)
+app.listen(3000);
